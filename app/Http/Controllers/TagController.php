e03e44920581bf08e5364aa9545fe663114bebf1
shunsuke-mizoguchi <shunsuke.m.1993@gmail.com>
2026-08-02 09:57:08 +0900

    refactor: 管理画面用コントローラーの配置を修正 #28

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreTagRequest;
 use App\Http\Requests\UpdateTagRequest;
 use App\Models\Tag;
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\TagController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 /*
